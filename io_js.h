634614278c6979d1508ba60110ae5fcfc80f0fc6
abstractsig <>
2020-06-30 11:13:27 +1200

    separate unit tests

--- io_js.h
+++ io_js.h
@@ -24,7 +24,7 @@ int fesetround (int rdir);
 
 #define qjsrt_printf(rt,...)
 #define qjsrt_putchar(rt,c)
-#define qjsctx_printf(ctx,...)
+#define qjsctx_printf(ctx,...) io_printf (JS_GetIO(ctx),##__VA_ARGS__);
 #define qjsctx_putchar(ctx,c)
 
 #define qjsp_printf(ps,...)
@@ -33,12 +33,108 @@ int fesetround (int rdir);
 
 #include "quickjs.h"
 
-#ifdef IMPLEMENT_IO_CORE
+int io_quickjs_eval_buffer (JSContext*,const void*,int,const char*,int);
+
+#ifdef IMPLEMENT_IO_JS
 //-----------------------------------------------------------------------------
 //
 // implementation
 //
 //-----------------------------------------------------------------------------
+#include <io_device.h>
+
+/*
+ *-----------------------------------------------------------------------------
+ *
+ * print values to log stream
+ *
+ *-----------------------------------------------------------------------------
+ */
+JSValue
+io_quickjs_print (JSContext *ctx, JSValueConst this_val,int argc, JSValueConst *argv) {
+	io_socket_t *log = io_get_socket (JS_GetIO(ctx),IO_LOG_SOCKET);
+	io_encoding_t *print = io_socket_new_message (log);
+
+	if (print) {
+		const char *str;
+		int i;
+
+		for (i = 0; i < argc; i++) {
+			if (i != 0) {
+				io_encoding_append_byte (print,' ');
+			}
+				
+			str = JS_ToCString(ctx, argv[i]);
+			if (!str) {
+				goto exception;
+			}
+			
+			io_encoding_append_string (print,str,strlen(str));
+			JS_FreeCString(ctx, str);
+		}
+	
+		io_socket_send_message (log,print);
+	}
+	
+	return JS_UNDEFINED;
+
+exception:
+	io_socket_send_message (log,print);
+	return JS_EXCEPTION;
+}
+
+void
+io_quickjs_dump_error (JSContext *ctx) {
+	JSValue exception_val, val;
+	bool is_error;
+
+	exception_val = JS_GetException(ctx);
+	is_error = JS_IsError(ctx, exception_val);
+
+	if (!is_error) {
+		qjsctx_printf (ctx,"Throw: ");
+	}
+
+	io_quickjs_print (ctx, JS_NULL, 1, (JSValueConst*) &exception_val);
+	
+	if (is_error) {
+	  val = JS_GetPropertyStr(ctx, exception_val, "stack");
+	  if (!JS_IsUndefined(val)) {
+			const char *stack = JS_ToCString(ctx, val);
+			qjsctx_printf (ctx,"%s\n", stack);
+			JS_FreeCString(ctx, stack);
+	  }
+	  JS_FreeValue(ctx, val);
+	}
+
+	JS_FreeValue(ctx, exception_val);
+}
+
+int
+io_quickjs_eval_buffer (
+	JSContext *ctx,const void *buf,int buf_len,const char *name,int flags
+) {
+    JSValue val;
+    int ret;
+
+    val = JS_Eval(ctx, buf, buf_len, name, flags);
+    if (JS_IsException(val)) {
+        io_quickjs_dump_error (ctx);
+        ret = -1;
+    } else {
+        ret = 0;
+    }
+    JS_FreeValue(ctx, val);
+    return ret;
+}
+
+int
+io_quickjs_enqueue_task (JSContext *ctx,JSJobFunc *task_func,int argc,JSValueConst *argv) {
+	int r = JS_EnqueueJob(ctx,task_func,argc,argv);
+	signal_io_task_pending (JS_GetIO(ctx));
+	return r;
+}
+
 
 #include "quickjs.c"
 #include "cutils.c"
@@ -46,76 +142,46 @@ int fesetround (int rdir);
 #include "libregexp.c"
 #include "libunicode.c"
 
-#endif /* IMPLEMENT_IO_CORE */
-#ifdef IMPLEMENT_VERIFY_QUICKJS
-//-----------------------------------------------------------------------------
-//
-// implementation
-//
-//-----------------------------------------------------------------------------
-TEST_BEGIN(test_quickjs_create_1) {
-	memory_info_t bminfo_begin,bminfo_end;
-
-	JSRuntime *jsfrt;
-	JSContext *jsfctx;
-
-	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_begin);
-
-	jsfrt = JS_NewRuntime(TEST_IO);
-	jsfctx = JS_NewContextRaw(jsfrt);
-
-	JS_SetGCThreshold (jsfrt,-1);
-
-	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_end);
-	VERIFY(bminfo_end.used_bytes > bminfo_begin.used_bytes,NULL);
-
-	JS_FreeContext(jsfctx);
-	JS_FreeRuntime(jsfrt);
-
-	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_end);
-	VERIFY(bminfo_end.used_bytes == bminfo_begin.used_bytes,NULL);
-}
-TEST_END
-
-
-UNIT_SETUP(setup_quickjs_unit_test) {
-	io_value_memory_get_info (io_get_short_term_value_memory (TEST_IO),TEST_MEMORY_INFO);
-	io_byte_memory_get_info (io_get_byte_memory (TEST_IO),TEST_MEMORY_INFO + 1);
-	return VERIFY_UNIT_CONTINUE;
-}
-
-UNIT_TEARDOWN(teardown_quickjs_unit_test) {
-	io_value_memory_t *vm = io_get_short_term_value_memory (TEST_IO);
-	memory_info_t bm_end,vm_end;
-	io_value_memory_do_gc (vm,-1);
-	io_value_memory_get_info (vm,&vm_end);
-	VERIFY (vm_end.used_bytes == TEST_MEMORY_INFO[0].used_bytes,NULL);
-	io_byte_memory_get_info (io_get_byte_memory (TEST_IO),&bm_end);
-	VERIFY (bm_end.used_bytes == TEST_MEMORY_INFO[1].used_bytes,NULL);
-}
-
-static void
-quickjs_unit_test (V_unit_test_t *unit) {
-	static V_test_t const tests[] = {
-		test_quickjs_create_1,
-		0
-	};
-	unit->name = "quickjs";
-	unit->description = "quickjs unit test";
-	unit->tests = tests;
-	unit->setup = setup_quickjs_unit_test;
-	unit->teardown = teardown_quickjs_unit_test;
-}
-
-void
-run_ut_quickjs (V_runner_t *runner) {
-	static const unit_test_t test_set[] = {
-		quickjs_unit_test,
-		0
-	};
-	V_run_unit_tests(runner,test_set);
-}
-
-
-#endif /* IMPLEMENT_VERIFY_QUICKJS */
+#endif /* IMPLEMENT_IO_JS */
 #endif
+/*
+------------------------------------------------------------------------------
+This software is available under 2 licenses -- choose whichever you prefer.
+------------------------------------------------------------------------------
+ALTERNATIVE A - MIT License
+Copyright (c) 2020 Gregor Bruce
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+------------------------------------------------------------------------------
+ALTERNATIVE B - Public Domain (www.unlicense.org)
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+software, either in source code form or as a compiled binary, for any purpose,
+commercial or non-commercial, and by any means.
+In jurisdictions that recognize copyright laws, the author or authors of this
+software dedicate any and all copyright interest in the software to the public
+domain. We make this dedication for the benefit of the public at large and to
+the detriment of our heirs and successors. We intend this dedication to be an
+overt act of relinquishment in perpetuity of all present and future rights to
+this software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------
+*/
---
+++ io_js_verify.h
@@ -0,0 +1,171 @@
+/*
+ *
+ * importing quickjs-2020-04-12 
+ *
+ * LICENSE
+ * =======
+ * See end of file for license terms.
+ *
+ */
+#ifndef io_js_verify_H_
+#define io_js_verify_H_
+#include <io_verify.h>
+#include "io_js.h"
+#ifdef IMPLEMENT_VERIFY_QUICKJS
+//-----------------------------------------------------------------------------
+//
+// implementation
+//
+//-----------------------------------------------------------------------------
+TEST_BEGIN(test_quickjs_create_1) {
+	memory_info_t bminfo_begin,bminfo_end;
+
+	JSRuntime *jsfrt;
+	JSContext *jsfctx;
+
+	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_begin);
+
+	jsfrt = JS_NewRuntime(TEST_IO);
+	jsfctx = JS_NewContextRaw(jsfrt);
+
+	JS_SetGCThreshold (jsfrt,-1);
+	
+	JS_AddIntrinsicBaseObjects (jsfctx);
+	JS_AddIntrinsicJSON (jsfctx);
+	JS_AddIntrinsicRegExpCompiler (jsfctx);
+	JS_AddIntrinsicRegExp (jsfctx);
+	JS_AddIntrinsicPromise (jsfctx);
+	JS_AddIntrinsicTypedArrays (jsfctx);
+	JS_AddIntrinsicProxy (jsfctx);
+	JS_AddIntrinsicEval (jsfctx);
+
+	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_end);
+	VERIFY(bminfo_end.used_bytes > bminfo_begin.used_bytes,NULL);
+
+	JS_FreeContext(jsfctx);
+	JS_FreeRuntime(jsfrt);
+
+	io_log_flush(TEST_IO);
+	
+	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_end);
+	VERIFY(bminfo_end.used_bytes == bminfo_begin.used_bytes,NULL);
+}
+TEST_END
+
+TEST_BEGIN(test_quickjs_eval_1) {
+	memory_info_t bminfo_begin,bminfo_end;
+
+	JSRuntime *jsfrt;
+	JSContext *jsfctx;
+	const char *begin = ""
+		"console.log('hello');\n"
+	;
+ 
+	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_begin);
+
+	jsfrt = JS_NewRuntime(TEST_IO);
+	jsfctx = JS_NewContextRaw(jsfrt);
+
+	JS_SetGCThreshold (jsfrt,-1);
+	
+	JS_AddIntrinsicBaseObjects (jsfctx);
+	JS_AddIntrinsicEval (jsfctx);
+
+	io_quickjs_eval_buffer (jsfctx,begin,strlen(begin),"<test>",0);
+
+	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_end);
+	VERIFY(bminfo_end.used_bytes > bminfo_begin.used_bytes,NULL);
+
+	JS_FreeContext(jsfctx);
+	JS_FreeRuntime(jsfrt);
+
+	io_log_flush(TEST_IO);
+	
+	io_byte_memory_get_info (io_get_byte_memory(TEST_IO),&bminfo_end);
+	VERIFY(bminfo_end.used_bytes == bminfo_begin.used_bytes,NULL);
+}
+TEST_END
+
+
+UNIT_SETUP(setup_quickjs_unit_test) {
+	io_value_memory_get_info (io_get_short_term_value_memory (TEST_IO),TEST_MEMORY_INFO);
+	io_byte_memory_get_info (io_get_byte_memory (TEST_IO),TEST_MEMORY_INFO + 1);
+	return VERIFY_UNIT_CONTINUE;
+}
+
+UNIT_TEARDOWN(teardown_quickjs_unit_test) {
+	io_value_memory_t *vm = io_get_short_term_value_memory (TEST_IO);
+	memory_info_t bm_end,vm_end;
+	io_value_memory_do_gc (vm,-1);
+	io_value_memory_get_info (vm,&vm_end);
+	VERIFY (vm_end.used_bytes == TEST_MEMORY_INFO[0].used_bytes,NULL);
+	io_byte_memory_get_info (io_get_byte_memory (TEST_IO),&bm_end);
+	VERIFY (bm_end.used_bytes == TEST_MEMORY_INFO[1].used_bytes,NULL);
+}
+
+static void
+quickjs_unit_test (V_unit_test_t *unit) {
+	static V_test_t const tests[] = {
+		test_quickjs_create_1,
+		test_quickjs_eval_1,
+		0
+	};
+	unit->name = "quickjs";
+	unit->description = "quickjs unit test";
+	unit->tests = tests;
+	unit->setup = setup_quickjs_unit_test;
+	unit->teardown = teardown_quickjs_unit_test;
+}
+
+void
+run_ut_quickjs (V_runner_t *runner) {
+	static const unit_test_t test_set[] = {
+		quickjs_unit_test,
+		0
+	};
+	V_run_unit_tests(runner,test_set);
+}
+
+#endif /* IMPLEMENT_VERIFY_QUICKJS */
+#endif
+/*
+------------------------------------------------------------------------------
+This software is available under 2 licenses -- choose whichever you prefer.
+------------------------------------------------------------------------------
+ALTERNATIVE A - MIT License
+Copyright (c) 2020 Gregor Bruce
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+------------------------------------------------------------------------------
+ALTERNATIVE B - Public Domain (www.unlicense.org)
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+software, either in source code form or as a compiled binary, for any purpose,
+commercial or non-commercial, and by any means.
+In jurisdictions that recognize copyright laws, the author or authors of this
+software dedicate any and all copyright interest in the software to the public
+domain. We make this dedication for the benefit of the public at large and to
+the detriment of our heirs and successors. We intend this dedication to be an
+overt act of relinquishment in perpetuity of all present and future rights to
+this software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------
+*/
